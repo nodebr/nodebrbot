efba030c5bb23e3cf83f63f34254d932e05e794b
Osvaldo Zonetti <zonetti@users.noreply.github.com>
2013-12-08 01:31:40 -0200

    atualizado exemplo

--- src/commands/hello.js
+++ src/commands/hello.js
@@ -1,14 +1,15 @@
 /*
- * Este comando é apenas um exemplo de como deve ser
- * a estrutura de um comando.
+ * Este comando é apenas um exemplo de como um comando
+ * deve ser estruturado.
  *
- * IMPORTANTE: Todo comando deve ter um arquivo de test.
+ * IMPORTANTE: Todo comando deve ter um arquivo de teste
+ * correspondente.
  * Vide diretório test/ na raiz para referência.
  */
 
 /*
  * Caso o comando dependa de algum(ns) módulo(s)
- * agora é a hora para importá-lo(s).
+ * esta é a hora de importá-lo(s).
  *
  * Exemplo:
  *
@@ -17,24 +18,16 @@
 
 /*
  * Variáveis auxiliares também podem ser utilizadas.
- * 
- * Vamos usar uma neste exemplo:
  */
 
 var valorAuxiliar = 'Hello world';
 
 /*
- * Próximo passo é escolher um nome para o comando.
- * (Que não está sendo utilizado é claro)
- *
- * O nome deve ser tudo minúsculo, sem espaços ou
- * caracteres especiais. (vamos manter simples :O)
- *
  * IMPORTANTE: O nome do comando deve ser o mesmo
  * do arquivo criado para o mesmo.
  *
  * IMPORTANTE: Todos os comandos possuem a mesma
- * assinatura, sendo os parâmetros:
+ * assinatura de função, sendo os parâmetros:
  *
  *   bot: a instância do robô (utilizada para 
  *        enviar mensagens basicamente)
@@ -49,7 +42,7 @@ var valorAuxiliar = 'Hello world';
  *         Irá receber como "args": ['lorem', 'ipsum']
  *
  *   end: IMPORTANTE! Callback que DEVE ser chamado
- *        no final do comando. (os testes dependem disso)
+ *        no final do comando. (para fins de testes)
  *
  * IMPORTANTE: Para fins de testes, o comando só termina
  * quando o callback "end" é chamado, ou quando retorna false.
@@ -61,11 +54,10 @@ var hello = function(bot, data, args, end) {
 };
 
 /*
- * Abaixo devem ficar as exportações do comando.
+ * As exportações do módulo devem ficar ao final do arquivo.
  *
  * IMPORTANTE: O comando em si deve ser exportado como "run".
- * Se houver valores auxiliares, devem ser exportados com o
- * mesmo nome, para que os testes possam utilizar se necessário.
+ * Variáveis auxiliares também devem ser exportadas para fins de testes.
  */
 
 exports.run = hello;
