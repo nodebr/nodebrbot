7602781addf4f3fa0260da80c2ba54ce5bc91162
Alan Hoffmeister <alanhoffmeister@gmail.com>
2014-06-13 16:58:12 -0300

    Busca por pacotes usando o node-modules.com

--- src/commands/npm.js
+++ src/commands/npm.js
@@ -4,32 +4,67 @@
  */
 
 var request = require('request');
+var irc = require('irc');
 
 var npm = function(bot, data, nick, args, end) {
 
-  var module = args[0];
+  var command = args[0];
 
-  if (args.length > 1 || module === '') {
-    bot.message('Comando inválido. Exemplo: !npm express');
+  if (args.length < 1 || command === '') {
+    bot.message('Comando inválido. Exemplo: !npm show express');
     return false;
   }
 
-  request('https://registry.npmjs.org/' + module, function(err, headers, body) {
-    if (err || headers.statusCode !== 200) {
-      bot.message('Erro ao buscar módulo');
-      return false;
-    }
+  if(command === 'show')
+    request('https://registry.npmjs.org/' + args[1], function(err, headers, body) {
+      if (err || headers.statusCode !== 200) {
+        bot.message('Erro ao buscar módulo');
+        return end();
+      }
 
-    try {
-      var json = JSON.parse(body);
-      bot.message(module + ': ' + json.description);
-      bot.message('Para mais informações: https://npmjs.org/package/' + module);
-    } catch(err) {
-      bot.message('Erro ao buscar módulo');
-    }
+      try {
+        var json = JSON.parse(body);
+        bot.message(args[1] + ': ' + json.description);
+        bot.message('Para mais informações: https://npmjs.org/package/' + args[1]);
+      } catch(err) {
+        bot.message('Erro ao buscar módulo');
+      }
 
+      end();
+    });
+  else if(command === 'search'){
+    var encoded = encodeURIComponent(args.slice(1).join(' '));
+    request('http://node-modules.com/search.json?q=' + encoded, function(err, res, body){
+      if(err){
+        console.error(err ? err.stack : res.statusCode + ' status ao procurar.');
+        bot.message('Erro ao procurar por módulos');
+        return end();
+      }
+
+      var packages = JSON.parse(body);
+      var response;
+
+      (function loop(index){
+        if(index > 1)
+          packages.pop();
+
+        response = packages.map(function(pkg){
+          return pkg.name + irc.colors.wrap('bold', ' ★') + (pkg.stars || 0);
+        }).join(', ');
+
+        if(response.length > 512)
+          loop(++index);
+      })(0);
+
+      console.log(response);
+
+      bot.message(response);
+      end();
+    });
+  } else {
+    bot.message('Comando inválido, utilize !npm search <expressão> ou !npm show <pacote>');
     end();
-  });
+  }
 
 };
 
--- test/npm_test.js
+++ test/npm_test.js
@@ -6,22 +6,30 @@ describe('!npm', function() {
 
   it('!npm', function(done) {
     npm('', function(output) {
-      output.should.contain('Comando inválido. Exemplo: !npm express');
+      output.should.contain('Comando inválido. Exemplo: !npm show express');
       done();
     });
   });
 
   it('!npm lorem ipsum', function(done) {
     npm('lorem ipsum', function(output) {
-      output.should.contain('Comando inválido. Exemplo: !npm express');
+      output.should.contain('Comando inválido, utilize !npm search');
       done();
     });
   });
 
-  it('!npm easy-cache', function(done) {
-    npm('easy-cache', function(output) {
+  it('!npm show easy-cache', function(done) {
+    npm('show easy-cache', function(output) {
       output.should.contain('easy-cache:');
       output.should.contain('Para mais informações: https://npmjs.org/package/easy-cache');
+      done();
+    });
+  });
+
+  it('!npm search express', function(done) {
+    npm('search express', function(output) {
+      output.should.contain('express');
+      output.should.contain('★');
       done();
     });
   });
