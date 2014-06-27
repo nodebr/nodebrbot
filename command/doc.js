4dc4953763312e16a855357e528c01c652dc5fd2
yamadapc <tacla.yamada@gmail.com>
2014-06-27 20:24:38 -0300

    Adiciona o comando `!doc`.
    A arquitetura nova se mostrou extremamente problemática tanto em termos
    de organização e manutenção quanto em termos de testabilidade.
    
    Por mais que eu aprecie o uso do `cache` do require para comparlhar
    objetos através de módulos, é preciso perceber que da forma em que os
    comandos estão organizados agora, fica um pouco chato de escrever testes
    pra eles (uma coisa é dividir um EventEmitter e outra completamente
    diferente, é acoplar um único EventEmitter a todos os módulos na mão).
    
    Eu entendo o ganho de flexibilidade de fazer as coisas dessa forma, mas
    a lógica de comandos fica diretamente acoplada à lógica de IO. Mudanças
    no `bot`, no `helper` (me abisma um pouco que o acesso às respostas - o
    output _per-se_ das funções é feito por meio de um módulo chamado
    `helper`) ou no `EventEmitter` compartilhado vão fazer todos os comandos
    mudarem sua lógica de saída.
    
    Isso é um problema recorrente em codebases de node.js (acoplar a lógica
    com o IO), afinal, o método padrão de programação com callbacks
    sobrecarrega o input das funções (os parâmetros) com o output (pelos
    callbacks). A longo prazo isso tem custos grandes, o que explica a
    existência de alternativas como promessas ou fibers. Essa discussão não
    é diretamente ligada a o que estou criticando na arquitetura, mas ela
    carrega alguns pontos em comum.
    
    Gostaria de propor mudanças à organização (e as executar), se possível.
    
    Quanto ao código adicionado por esse commit, a ideia é relativamente
    simples:
    
    Um usuário manda a mensagem `!doc [token]` para o canal e o bot responde
    com a documentação trimada desse `token`. Um `token` seria algo como:
    `http.Server` ou `child_process`. O comando faz um request (que poderia
    facilmente ser evitado com caching diário ou algo assim, para
    performance) para `http://nodejs.org/api/module.json` e parseia o output
    fazendo uma busca simples pelo token.
    
    Hoje, ele pode buscar por métodos, classes, propriedades, eventos e
    seções (por exemplo: `vm.caveats`).
    
    Há coisas que eu gostaria de melhorar e discutir. O parsing de links não
    é feito da forma mais elegante possível e estou incerto sobre o tamanho
    máximo da documentação enviada. Mas só o fato de ele mandar o link já é
    um bom avanço e cobre a maior parte do use-case desse comando.
    
    Dê feedback, merge quando quiser, ainda posso mudar o que for preciso e
    adicionar mais coisas. Tentei me ater ao estilo proposto o máximo
    possível Tentei me ater ao estilo proposto o máximo possível, mas sei
    que tenho hábitos fortes de estilo, então qualquer coisa fora da norma
    ainda pode ser mudada.

---
+++ command/doc.js
@@ -0,0 +1,118 @@
+'use strict';
+var request = require('request');
+var md = require('html-md');
+var e = require(__dirname + '/../lib/event');
+var helper = require(__dirname + '/../lib/helper');
+
+var TRIM_LIMIT = 100;
+exports.TRIM_LIMIT = TRIM_LIMIT;
+
+var HOST = 'http://nodejs.org/api/';
+
+function serializeDoc(mainModule, name, mod) {
+    var intro = mod.type + ' ' + name + ' --> ';
+    var markdown = md(mod.desc).slice(0, TRIM_LIMIT) + '...';
+
+    // não consegui encontrar uma solução só com regexp que não inserisse um '_'
+    // desnecessário quando `mod.textRaw` terminasse em um caractere especial
+    // (especialmente, se os últimos dois caracteres forem especiais)
+    var section = mod.textRaw
+        .split(/[^a-zA-Z]+/)
+        .filter(function(x) {
+          return !!x;
+        })
+        .join('_')
+        .toLowerCase();
+
+    var link = HOST + mainModule + '.html#' + mainModule + '_' + section;
+
+    return intro + markdown.replace(/[\n\t]/g, ' ') + ' ' + link;
+}
+
+exports.serializeDoc = serializeDoc;
+
+function findWithName(arr, name) {
+    var el;
+    if(!arr) return;
+    for(var i = 0, len = arr.length; i < len; i++) {
+        el = arr[i];
+        if(el.name === name) return el;
+    }
+}
+
+exports.findWithName = findWithName;
+
+function dispatch(fns) {
+    var len = fns.length;
+
+    return function() {
+        var ret;
+        for(var i = 0; i < len; i++) {
+            ret = fns[i].apply(this, arguments);
+            if(ret) return ret;
+        }
+    };
+}
+
+exports.dispatch = dispatch;
+
+function findModule(json, paths, name) {
+    var ret = paths.slice(1).reduce(function(targetModule, path) {
+        // Veja M. Fogus "Functional Javascript" capítulo 5
+        var find = dispatch([
+            findWithName.bind(null, targetModule.modules),
+            findWithName.bind(null, targetModule.classes),
+            findWithName.bind(null, targetModule.methods),
+            findWithName.bind(null, targetModule.properties),
+            findWithName.bind(null, targetModule.events)
+        ]);
+        return find(name) || find(path) || targetModule;
+    }, json.modules[0]);
+
+    return ret;
+}
+
+exports.findModule = findModule;
+
+function doc(args, nick) {
+    var name = args._[0];
+    if(!name) {
+        return helper.say('Comando inválido. Exemplo: !doc http.createServer');
+    }
+    var paths = name.split('.');
+    var mainModule = paths[0];
+    var targetLink = HOST + mainModule + '.json';
+
+    request(targetLink, function(err, headers, body) {
+        if(err || headers.statusCode !== 200) {
+            helper.say('Erro ao buscar a documentação para ' + name);
+            return false;
+        }
+
+        var json = JSON.parse(body);
+
+        var targetModule = findModule(json, paths, name);
+
+        if(targetModule) {
+            var serializedDoc = serializeDoc(mainModule, name, targetModule);
+
+            helper.say(nick + ', ' + serializedDoc);
+        } else {
+            helper.say('A documentação para ' + name + ' não foi ' +
+                       'encontrada.');
+        }
+
+    });
+}
+
+exports.doc = doc;
+
+e.on('command.probe.doc', function(callback) {
+    callback(null, {
+        name: 'doc',
+        version: '0.0.1',
+        description: 'Pesquisa por um token na documentação do node.js core.'
+    });
+});
+
+e.on('command.exec.doc', doc);
