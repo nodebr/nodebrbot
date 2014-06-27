0c31249b1ca81586afea3edcb32530bb1ca1087d
Alan Hoffmeister <alanhoffmeister@gmail.com>
2014-06-27 11:19:17 -0300

    Removendo pasta que sobrou da 0.1.0-alpha

--- listener/irc-testing.js
+++
@@ -1,8 +0,0 @@
-// Arquivo usado para testes
-var e = require(__dirname + '/../lib/event');
-
-e.on('state.set.bot.online', function(){
-    e.emit('irc.getWhois', 'alanhoff', function(){
-        console.log(arguments);
-    });
-});
