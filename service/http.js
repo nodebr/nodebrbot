03e9aa3f00c25ae8c065495ac7c2ca84ff887927
Alan Hoffmeister <alanhoffmeister@gmail.com>
2014-06-30 19:07:33 -0300

    Serviço de HTTP

---
+++ service/http.js
@@ -0,0 +1,21 @@
+var e = require(__dirname + '/../lib/event');
+var config = require(__dirname + '/../lib/config');
+var express = require('express');
+var app = express();
+
+app.all('/api/v1/:module', function(req, res, next){
+    var method = req.method.toLowerString();
+    var listeners = e.listeners('http.v1.' + method + '.' + req.params.module);
+
+
+    if(!listeners || !listeners.length)
+        return next();
+
+    e.emit('http.v1.' + method + '.' + req.params.module, req, res, next);
+});
+
+app.all(function(req, res){
+    res.send(404, 'Rota não encontrada.');
+});
+
+app.listen(config.http.port);
